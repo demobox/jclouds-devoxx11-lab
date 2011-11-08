13aed7b294e76329d9f57a331203b2896cc4b3ff
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2011-11-08 20:04:54 +0000

    Example 1 updates - Split uploader into multiple methods - Use environment variables for credentials - Added 'C' version with file name for blob - Added a test class with test for each version

--- exercise1/src/main/java/com/devoxx/y2011/labs/jclouds/exercise1/ImageUploader.java
+++ exercise1/src/main/java/com/devoxx/y2011/labs/jclouds/exercise1/ImageUploader.java
@@ -1,7 +1,5 @@
 /*
- * @(#)BlobWriterReader.java     2 Nov 2011
- *
- * Copyright © 2010 Andrew Phillips.
+ * Copyright 2011 Andrew Phillips, Andrew Kennedy.
  *
  * ====================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,6 +20,7 @@ package com.devoxx.y2011.labs.jclouds.exercise1;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 
 import org.jclouds.blobstore.BlobStore;
 import org.jclouds.blobstore.BlobStoreContext;
@@ -34,35 +33,48 @@ import com.google.common.collect.ImmutableSet;
 /**
  * @author aphillips
  * @since 2 Nov 2011
- *
  */
 public class ImageUploader {
+    public static final String CONTAINER_NAME = "test-container-1";
+    public static final String BLOB_NAME = "uploadedImage";
+
     private final BlobStoreContext ctx;
+    private final BlobStore store;
     
     public ImageUploader(String provider, String identity, String credential) {
-        ctx = new BlobStoreContextFactory().createContext(provider, identity, credential, 
-                ImmutableSet.of(new Log4JLoggingModule()));
+        ctx = new BlobStoreContextFactory().createContext(provider, identity, credential, ImmutableSet.of(new Log4JLoggingModule()));
+        System.out.printf("Provider '%s' uses %s consistency%n", provider, ctx.getConsistencyModel());
+        store = ctx.getBlobStore();
     }
     
     public void uploadImage(File image) throws IOException {
-        BlobStore store = ctx.getBlobStore();
-        final String containerName = "test-container-1";
-        final String blobName = "uploadedImage";
-        System.out.format("Creating public container '%s'%n", containerName);
-        store.createContainerInLocation(null, containerName, CreateContainerOptions.Builder.publicRead());
-        store.putBlob(containerName, store.blobBuilder(blobName).payload(image).build());
-        System.out.format("Now please open '%s' in a browser%nPress any key, then <enter>: ", 
-                store.blobMetadata(containerName, blobName).getPublicUri());
-        // pause
-        System.in.read();
-        tryDeleteContainer(store, containerName);
+        createContainer(CONTAINER_NAME);
+        storeBlob(CONTAINER_NAME, BLOB_NAME, image);
+        System.out.printf("Now please open '%s' in a browser%nPress any key, then <enter>: ", getImageUri().toASCIIString());
+        System.in.read(); // pause
+        tryDeleteContainer(CONTAINER_NAME);
     }
-    
-    private static void tryDeleteContainer(BlobStore store, String containerName) {
-        try {
+
+    public void createContainer(String containerName) {
+        System.out.printf("Creating public container '%s'%n", containerName);
+        store.createContainerInLocation(null, containerName, CreateContainerOptions.Builder.publicRead());
+    }
+
+    public void storeBlob(String containerName, String blobName, File image) {
+        System.out.printf("Storing file '%s' as '%s'%n", image.getPath(), blobName);
+        store.putBlob(containerName, store.blobBuilder(blobName).payload(image).build());
+    }
+
+    public URI getImageUri() {
+        return store.blobMetadata(CONTAINER_NAME, BLOB_NAME).getPublicUri();
+    }
+         
+    public void tryDeleteContainer(String containerName) {
+        System.out.printf("Deleting public container '%s'%n", containerName);
+             try {
             store.deleteContainer(containerName);
-        } catch (Exception exception) {
-            System.err.format("Unable to delete container due to: %s%n", exception.getMessage());
+             } catch (Exception exception) {
+            System.err.printf("Unable to delete container due to: %s%n", exception.getMessage());
         }
     }
     
@@ -72,7 +84,7 @@ public class ImageUploader {
     
     public static void main(String[] args) throws IOException {
         if (args.length < 3) {
-            System.out.format("%nUsage: %s <provider> <identity> <credential>%n", ImageUploader.class.getSimpleName());
+            System.out.printf("%nUsage: %s <provider> <identity> <credential>%n", ImageUploader.class.getSimpleName());
             System.exit(1);
         }
         ImageUploader uploader = new ImageUploader(args[0], args[1], args[2]);
